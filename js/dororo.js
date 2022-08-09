28a895f2cee99f828c8c1b686508a38a5c9cef91
Irfan Shadik <77362427+irfanshadikofficial@users.noreply.github.com>
2022-08-10 01:02:36 +0600

    v0.0.1

---
+++ js/dororo.js
@@ -0,0 +1,98 @@
+let notice = ' | [The Ads provided are from gogoanime, We are not related in this.]'
+function eps1(){
+    document.getElementById('player').src = 'https://goload.io/streaming.php?id=MTY2NDQ4&title=Dororo+%28Dub%29+Episode+1';
+    document.getElementById('epsno').innerHTML = 'Episode 1' + notice;
+}
+function eps2(){
+    document.getElementById('player').src = 'https://goload.io/streaming.php?id=MTY2NDQ5&title=Dororo+%28Dub%29+Episode+2';
+    document.getElementById('epsno').innerHTML = 'Episode 2' + notice;
+}
+function eps3(){
+    document.getElementById('player').src = 'https://goload.io/streaming.php?id=MTY2NDUw&title=Dororo+%28Dub%29+Episode+3';
+    document.getElementById('epsno').innerHTML = 'Episode 3' + notice;
+}
+function eps4(){
+    document.getElementById('player').src = 'https://goload.io/streaming.php?id=MTY2NDUx&title=Dororo+%28Dub%29+Episode+4';
+    document.getElementById('epsno').innerHTML = 'Episode 4' + notice;
+}
+function eps5(){
+    document.getElementById('player').src = 'https://goload.io/streaming.php?id=MTY2NDUy&title=Dororo+%28Dub%29+Episode+5';
+    document.getElementById('epsno').innerHTML = 'Episode 5' + notice;
+}
+function eps6(){
+    document.getElementById('player').src = 'https://goload.io/streaming.php?id=MTY2NDUz&title=Dororo+%28Dub%29+Episode+6';
+    document.getElementById('epsno').innerHTML = 'Episode 6' + notice;
+}
+function eps7(){
+    document.getElementById('player').src = 'https://goload.io/streaming.php?id=MTY2NDU0&title=Dororo+%28Dub%29+Episode+7';
+    document.getElementById('epsno').innerHTML = 'Episode 7' + notice;
+}
+function eps8(){
+    document.getElementById('player').src = 'https://goload.io/streaming.php?id=MTY2NDU1&title=Dororo+%28Dub%29+Episode+8';
+    document.getElementById('epsno').innerHTML = 'Episode 8' + notice;
+}
+function eps9(){
+    document.getElementById('player').src = 'https://goload.io/streaming.php?id=MTY2NDU2&title=Dororo+%28Dub%29+Episode+9';
+    document.getElementById('epsno').innerHTML = 'Episode 9' + notice;
+}
+function eps10(){
+    document.getElementById('player').src = 'https://goload.io/streaming.php?id=MTY2NDU3&title=Dororo+%28Dub%29+Episode+10';
+    document.getElementById('epsno').innerHTML = 'Episode 10' + notice;
+}
+function eps11(){
+    document.getElementById('player').src = 'https://goload.io/streaming.php?id=MTY2NDU4&title=Dororo+%28Dub%29+Episode+11';
+    document.getElementById('epsno').innerHTML = 'Episode 11' + notice;
+}
+function eps12(){
+    document.getElementById('player').src = 'https:////goload.io/streaming.php?id=MTY2NDU5&title=Dororo+%28Dub%29+Episode+12';
+    document.getElementById('epsno').innerHTML = 'Episode 12' + notice;
+}
+function eps13(){
+    document.getElementById('player').src = 'https://goload.io/streaming.php?id=MTY2NDYw&title=Dororo+%28Dub%29+Episode+13';
+    document.getElementById('epsno').innerHTML = 'Episode 13' + notice;
+}
+function eps14(){
+    document.getElementById('player').src = 'https://goload.io/streaming.php?id=MTY2NDYx&title=Dororo+%28Dub%29+Episode+14';
+    document.getElementById('epsno').innerHTML = 'Episode 14' + notice;
+}
+function eps15(){
+    document.getElementById('player').src = 'https://goload.io/streaming.php?id=MTY2NDYy&title=Dororo+%28Dub%29+Episode+15';
+    document.getElementById('epsno').innerHTML = 'Episode 15' + notice;
+}
+function eps16(){
+    document.getElementById('player').src = 'https://goload.io/streaming.php?id=MTY2NDYz&title=Dororo+%28Dub%29+Episode+16';
+    document.getElementById('epsno').innerHTML = 'Episode 16' + notice;
+}
+function eps17(){
+    document.getElementById('player').src = 'https://goload.io/streaming.php?id=MTY2NDY0&title=Dororo+%28Dub%29+Episode+17';
+    document.getElementById('epsno').innerHTML = 'Episode 17' + notice;
+}
+function eps18(){
+    document.getElementById('player').src = 'https://goload.io/streaming.php?id=MTY2NDY1&title=Dororo+%28Dub%29+Episode+18';
+    document.getElementById('epsno').innerHTML = 'Episode 18' + notice;
+}
+function eps19(){
+    document.getElementById('player').src = 'https://goload.io/streaming.php?id=MTY2NDY2&title=Dororo+%28Dub%29+Episode+19';
+    document.getElementById('epsno').innerHTML = 'Episode 19' + notice;
+}
+function eps20(){
+    document.getElementById('player').src = 'https://goload.io/streaming.php?id=MTY2NDY3&title=Dororo+%28Dub%29+Episode+20';
+    document.getElementById('epsno').innerHTML = 'Episode 20' + notice;
+}
+function eps21(){
+    document.getElementById('player').src = 'https://goload.io/streaming.php?id=MTY2NDY4&title=Dororo+%28Dub%29+Episode+21';
+    document.getElementById('epsno').innerHTML = 'Episode 21' + notice;
+}
+function eps22(){
+    document.getElementById('player').src = 'https://goload.io/streaming.php?id=MTY2NDY5&title=Dororo+%28Dub%29+Episode+22';
+    document.getElementById('epsno').innerHTML = 'Episode 22' + notice;
+}
+function eps23(){
+    document.getElementById('player').src = 'https://goload.io/streaming.php?id=MTY2NDcw&title=Dororo+%28Dub%29+Episode+23';
+    document.getElementById('epsno').innerHTML = 'Episode 23' + notice;
+}
+function eps24(){
+    document.getElementById('player').src = 'https://goload.io/streaming.php?id=MTY2NDcx&title=Dororo+%28Dub%29+Episode+24';
+    document.getElementById('epsno').innerHTML = 'Episode 24' + notice;
+}
+
